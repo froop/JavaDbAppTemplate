8e920400c4ba5a816cb51e9d7ed719889c7efdd8
froop <frooprogrammer@gmail.com>
2014-05-15 12:01:43 +0900

    refactoring Sample to use default table/column name

--- src/main/java/froop/db/jpa/Sample.java
+++ src/main/java/froop/db/jpa/Sample.java
@@ -6,16 +6,16 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name="sample")
+@Table
 @Data
 public class Sample implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
-  @Column(name="id")
+  @Column
   private long id;
 
-  @Column(name="name")
+  @Column
   private String name;
 
   protected Sample() {}
