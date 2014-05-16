cf7ab4ea288ac55c7d940ae0e9212eeb0442eee4
froop <frooprogrammer@gmail.com>
2014-05-17 07:28:09 +0900

    inline JdbcSample <- DerbyConnectionFactory

--- src/main/java/froop/db/jdbc/JdbcSample.java
+++ src/main/java/froop/db/jdbc/JdbcSample.java
@@ -1,23 +1,18 @@
 package froop.db.jdbc;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Optional;
 
 /**
  * JDBCでデータベースに直接アクセスするサンプル.
  */
 public class JdbcSample {
+  private static final String DB_URL = "jdbc:derby:data/derby/sample";
   private static final String SQL_SELECT = "SELECT name FROM sample WHERE id=?";
-  private static final String SQL_UPDATE = "UPDATE sample SET name=?  WHERE id=?";
-
-  private final DerbyConnectionFactory factory = new DerbyConnectionFactory(
-      "jdbc:derby:data/derby/sample");
+  private static final String SQL_UPDATE = "UPDATE sample SET name=? WHERE id=?";
 
   public Optional<String> selectNameById(long id) throws SQLException {
-    try (Connection conn = factory.getConnection();
+    try (Connection conn = DriverManager.getConnection(DB_URL);
         PreparedStatement stmt = conn.prepareStatement(SQL_SELECT)) {
       stmt.setLong(1, id);
       try (ResultSet rs = stmt.executeQuery()) {
@@ -31,7 +26,7 @@ public class JdbcSample {
   }
 
   public void update(long id, String name) throws SQLException {
-    try (Connection conn = factory.getConnection();
+    try (Connection conn = DriverManager.getConnection(DB_URL);
          PreparedStatement stmt = conn.prepareStatement(SQL_UPDATE)) {
       stmt.setString(1, name);
       stmt.setLong(2, id);
