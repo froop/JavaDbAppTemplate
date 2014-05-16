0a695c5e76cf5edb0a2e552be48768c021a144ce
froop <frooprogrammer@gmail.com>
2014-05-17 07:17:07 +0900

    delete derby.log setting

--- src/main/java/froop/db/jdbc/DerbyConnectionFactory.java
+++ src/main/java/froop/db/jdbc/DerbyConnectionFactory.java
@@ -1,22 +1,11 @@
 package froop.db.jdbc;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Apache Derby を Embedded モード(同一プロセス内で起動)で使用する.
- * 参考: http://db.apache.org/derby/
- */
 public class DerbyConnectionFactory {
   private final String connectUrl;
-
-  static {
-    final String logDir = "logs";
-    new File(logDir).mkdir();
-    System.setProperty("derby.stream.error.file", logDir + "/derby.log");
-  }
 
   /**
    * @param connectUrl JDBC用のデータベース接続URL
