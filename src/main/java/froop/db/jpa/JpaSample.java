9ba81b8e863a28a176616fdc60727bd4fd0afc01
froop <frooprogrammer@gmail.com>
2014-05-20 09:59:29 +0900

    rename

--- src/main/java/froop/db/jpa/JpaSample.java
+++ src/main/java/froop/db/jpa/JpaSample.java
@@ -16,7 +16,7 @@ import java.util.Optional;
  */
 public class JpaSample implements SampleData {
 
-  @PersistenceContext(unitName = "sample")
+  @PersistenceContext(unitName = "jpa-sample")
   private EntityManager entityManager;
 
   public JpaSample() {
