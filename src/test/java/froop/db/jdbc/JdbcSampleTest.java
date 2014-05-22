23c075acea137329afd24a236989ee72cccc20c6
froop <frooprogrammer@gmail.com>
2014-05-22 14:34:32 +0900

    inline SampleTestBase

--- src/test/java/froop/db/jdbc/JdbcSampleTest.java
+++ src/test/java/froop/db/jdbc/JdbcSampleTest.java
@@ -1,12 +1,54 @@
 package froop.db.jdbc;
 
-import froop.db.test_common.SampleTestBase;
+import froop.db.test_common.DerbyDBUnit;
+import froop.domain.SampleData;
+import froop.domain.SampleValue;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
-public class JdbcSampleTest extends SampleTestBase {
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class JdbcSampleTest {
+
+  @Rule
+  public final DerbyDBUnit dbUnit;
+
+  protected SampleData target;
+
+  public JdbcSampleTest() {
+    this.dbUnit = DerbyDBUnit.xmlDataSetOf("jdbc:derby:data/derby/sample", toStream("Sample.xml"));
+  }
 
   @Before
   public void setUp() throws Exception {
     target = new JdbcSample();
+  }
+
+  @Test
+  public void testQueryAll() throws SQLException {
+    assertThat(target.queryAll(), is(Arrays.asList(
+        SampleValue.of(1, "name1"), SampleValue.of(2, "name2"))));
+  }
+
+  @Test
+  public void testQueryById() throws SQLException {
+    assertThat(target.queryById(1).get(), is(SampleValue.of(1, "name1")));
+  }
+
+  @Test
+  public void testUpdate() throws Exception {
+    target.update(SampleValue.of(1, "name1b"));
+
+    dbUnit.assertEqualsTable(toStream("SampleUpdate.xml"), "sample");
+  }
+
+  private InputStream toStream(String fileName) {
+    return getClass().getResourceAsStream(fileName);
   }
 }
--- src/test/java/froop/db/jpa/JpaSampleTest.java
+++ src/test/java/froop/db/jpa/JpaSampleTest.java
@@ -1,19 +1,38 @@
 package froop.db.jpa;
 
-import froop.db.test_common.SampleTestBase;
+import froop.db.test_common.DerbyDBUnit;
+import froop.domain.SampleData;
+import froop.domain.SampleValue;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Arrays;
 
-public class JpaSampleTest extends SampleTestBase {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class JpaSampleTest {
   private static final EntityManagerFactory FACTORY =
       Persistence.createEntityManagerFactory("jpa-sample");
 
   private EntityManager entityManager;
+
+  @Rule
+  public final DerbyDBUnit dbUnit;
+
+  protected SampleData target;
+
+  public JpaSampleTest() {
+    this.dbUnit = DerbyDBUnit.xmlDataSetOf("jdbc:derby:data/derby/sample", toStream("Sample.xml"));
+  }
 
   @Before
   public void setUp() throws Exception {
@@ -26,11 +45,29 @@ public class JpaSampleTest extends SampleTestBase {
     entityManager.close();
   }
 
-  @Override
-  public void doUpdate() {
+  @Test
+  public void testQueryAll() throws SQLException {
+    assertThat(target.queryAll(), is(Arrays.asList(
+        SampleValue.of(1, "name1"), SampleValue.of(2, "name2"))));
+  }
+
+  @Test
+  public void testQueryById() throws SQLException {
+    assertThat(target.queryById(1).get(), is(SampleValue.of(1, "name1")));
+  }
+
+  @Test
+  public void testUpdate() throws Exception {
     EntityTransaction tran = entityManager.getTransaction();
+
     tran.begin();
-    super.doUpdate();
+    target.update(SampleValue.of(1, "name1b"));
     tran.commit();
+
+    dbUnit.assertEqualsTable(toStream("SampleUpdate.xml"), "sample");
+  }
+
+  private InputStream toStream(String fileName) {
+    return getClass().getResourceAsStream(fileName);
   }
 }
