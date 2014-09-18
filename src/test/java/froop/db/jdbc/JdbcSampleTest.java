b075f4069a733fc0f1e58e8fce8baae1d322b05d
froop <frooprogrammer@gmail.com>
2014-09-18 10:29:41 +0900

    refactoring JdbcSampleTest extends SampleTestBase

--- src/test/java/froop/db/jdbc/JdbcSampleTest.java
+++ src/test/java/froop/db/jdbc/JdbcSampleTest.java
@@ -1,67 +1,21 @@
 package froop.db.jdbc;
 
-import froop.db.test_common.DerbyDBUnit;
-import froop.domain.SampleData;
-import froop.domain.SampleValue;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
 
-import java.io.InputStream;
-import java.sql.SQLException;
-import java.util.Arrays;
+import froop.db.base.SampleTestBase;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-
-public class JdbcSampleTest {
-
-  @Rule
-  public final DerbyDBUnit dbUnit;
-
-  protected SampleData target;
-
-  public JdbcSampleTest() {
-    this.dbUnit = DerbyDBUnit.xmlDataSetOf("jdbc:derby:data/derby/sample", toStream("Sample.xml"));
-  }
+public class JdbcSampleTest extends SampleTestBase {
 
   @Before
   public void setUp() throws Exception {
     target = new JdbcSample();
   }
 
-  @Test
-  public void testQueryAll() throws SQLException {
-    assertThat(target.queryAll(), is(Arrays.asList(
-        SampleValue.of(1, "name1"), SampleValue.of(2, "name2"))));
+  @Override
+  protected void beginTransaction() {
   }
 
-  @Test
-  public void testQueryById() throws SQLException {
-    assertThat(target.queryById(1).get(), is(SampleValue.of(1, "name1")));
-  }
-
-  @Test
-  public void testQueryById_NotExists() throws SQLException {
-    assertFalse(target.queryById(9).isPresent());
-  }
-
-  @Test
-  public void testUpdate() throws Exception {
-    target.update(SampleValue.of(1, "name1b"));
-
-    dbUnit.assertEqualsTable(toStream("SampleUpdate.xml"), "sample");
-  }
-
-  @Test
-  public void testUpdate_NotExists() throws Exception {
-    target.update(SampleValue.of(9, "name9"));
-
-    dbUnit.assertEqualsTable(toStream("Sample.xml"), "sample");
-  }
-
-  private InputStream toStream(String fileName) {
-    return getClass().getResourceAsStream(fileName);
+  @Override
+  protected void commitTransaction() {
   }
 }
